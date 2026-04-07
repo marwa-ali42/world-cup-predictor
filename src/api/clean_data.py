3310e0271afe194e3fa3207570a081bbf04d33fd
Hamda <marwa_ali42@outlook.com>
2026-04-07 20:32:02 +0100

    feat: added raw data, filtered out pre 2016 games and missing scores

---
+++ src/api/clean_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+raw_data = pd.read_csv('~/projects/world-cup-predictor/data/raw/results.csv')
+
+filtered_data = raw_data[raw_data['date'] >= '2016-01-01']
+filtered_data = filtered_data.dropna(subset=['home_score', 'away_score'], how='any')
+filtered_data = filtered_data[['date', 'tournament', 'home_team', 'away_team',
+                               'home_score', 'away_score', 'neutral']]
+print(filtered_data.head())
+print(filtered_data.shape)
+print(filtered_data.tail())
+
+filtered_data.to_csv('~/projects/world-cup-predictor/data/processed/clean_results.csv', index=False)
