8bd1932c186a7505f97e0e11d4cc61f68ce9f6ac
Hamda <marwa_ali42@outlook.com>
2026-04-14 18:17:21 +0100

    feat: calculated probability each team wins world cup using monte carlo simulation

--- src/simulation/tournament.py
+++ src/simulation/tournament.py
@@ -10,25 +10,34 @@ actual_ratings = run_elo(df)
 
 def simulate_round(teams, ratings):
     winners = []
-    for i in range(0, len(teams), 2):
-        winner = simulate_match(teams[i], teams[i+1], ratings)
+    no_of_teams = len(teams)
+    for i in range(0, no_of_teams//2):
+        winner = simulate_match(teams[i], teams[no_of_teams-i-1], ratings)
         winners.append(winner)
     return winners
 
-sorted_teams = sorted(actual_ratings.items(), key= lambda x:x[1], reverse=True)[:16]
-actual_teams = [team for team, _ in sorted_teams]
-print(simulate_round(actual_teams, actual_ratings))
+def seed_teams():
+    sorted_teams = sorted(actual_ratings.items(), key= lambda x:x[1], reverse=True)[:16]
+    return sorted_teams
+
+actual_teams = [team for team, _ in seed_teams()]
+print(f"Simulating a round gives results: {simulate_round(actual_teams, actual_ratings)}")
 
 def simulate_tournament(teams, ratings):
     while (len(teams)>1):
         teams = simulate_round(teams, ratings)
-        print(teams)
+        # print(teams)
     return teams[0]
-print(simulate_tournament(actual_teams, actual_ratings))
+print(f"Simulating a tournament gives result: {simulate_tournament(actual_teams, actual_ratings)}")
 
 results = Counter()
 
-for i in range(10000):
-    winner = simulate_tournament(actual_teams, actual_ratings)
+number_of_tournaments = 10000
+for i in range(number_of_tournaments):
+    winner = simulate_tournament(actual_teams.copy(), actual_ratings)
     results[winner] += 1
-print(results)
+print(f"Simulating {number_of_tournaments} tournaments gives result: {results}")
+
+print(f"For each team, probability of winning the world cup, calculated using {number_of_tournaments} simulations, is: ")
+for team, count in sorted(results.items(), key=lambda x:x[1], reverse=True):
+    print(f"{team}: {count/number_of_tournaments *100}%")
