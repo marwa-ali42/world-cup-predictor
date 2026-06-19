adf378590f44b89ea87e5573a8a71f9f4e4e8bd7
Hamda <marwa_ali42@outlook.com>
2026-06-19 22:43:07 +0100

    feat: update data to include games up until WC start

--- src/api/clean_data.py
+++ src/api/clean_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 raw_data = pd.read_csv('~/projects/world-cup-predictor/data/raw/results.csv')
 
 filtered_data = raw_data[raw_data['date'] >= '2016-01-01']
+filtered_data = filtered_data[filtered_data['date'] < '2026-06-11']
 filtered_data = filtered_data.dropna(subset=['home_score', 'away_score'], how='any')
 filtered_data = filtered_data[['date', 'tournament', 'home_team', 'away_team',
                                'home_score', 'away_score', 'neutral']]
